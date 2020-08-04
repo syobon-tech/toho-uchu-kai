fc7764ecfda292b5c7f1984d6f63d9b6de9aabb1
syobon-tech <syobon_tech@outlook.com>
2020-08-04 12:09:44 +0900

    フレームレート表示

---
+++ Assets/Scripts/FramerateCalucrator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FramerateCalucrator : MonoBehaviour
+{
+    public Text text;
+    int frameCount;
+    float prevTime;
+    float fps;
+
+    void Start()
+    {
+        frameCount = 0;
+        prevTime = 0.0f;
+    }
+
+    void Update()
+    {
+        frameCount++;
+        float time = Time.realtimeSinceStartup - prevTime;
+
+        if (time >= 0.5f) {
+            fps = frameCount / time;
+            Debug.Log(fps);
+
+            frameCount = 0;
+            prevTime = Time.realtimeSinceStartup;
+        }
+        text.text = fps.ToString();
+    }
+}
